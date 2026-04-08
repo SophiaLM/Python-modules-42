5773a10f099cf2c0907af8fee4b6d607ad9b2edf
SophiaLM <106804291+SophiaLM@users.noreply.github.com>
2026-04-09 00:17:59 +0200

    Add files via upload

--- Python_03/ex1/ft_score_analytics.py
+++ Python_03/ex1/ft_score_analytics.py
@@ -7,21 +7,32 @@ def validate_arg() -> None | list:
     Validate arg and return it into a list
     """
     scores = []
+    invalid_argv = []
 
     for arg in sys.argv[1:]:
         try:
             scores.append(int(arg))
         except ValueError:
-            print("Error: All arguments must be numbers")
-            return None
+            invalid_argv.append(arg)
+            continue
+    len_invalid_argv = len(invalid_argv)
+    if len_invalid_argv == len(sys.argv):
+        print("No scores provided. Usage:"
+              " python3 ft_score_analytics.py <score1> <score2> ...")
+        return None
+    if len_invalid_argv > 0:
+        print(f"Invalid argument: {invalid_argv}")
+        print("No scores provided. Usage:"
+              " python3 ft_score_analytics.py <score1> <score2> ...")
+        return None
     return (scores)
 
 
-def score_analitics(scores: list) -> None:
+def score_analitics(scores: list[int]) -> None:
     """
     Calculate and display detailed statistics from a list of scores.
     """
-    num_players = len(sys.argv) - 1
+    num_players = len(scores)
     total_scores = sum(scores)
     avarage_score = total_scores / num_players
     high_score = max(scores)
